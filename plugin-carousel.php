dcecb9b42f21e71019aeb2aa44120ea9d75a3749
rod773 <rodrigoarcadio1@gmail.com>
2024-08-16 14:36:19 +0200

    initial

---
+++ plugin-carousel.php
@@ -0,0 +1,31 @@
+<?php
+/*
+Plugin Name: Plugin Carousel Bootstrap
+Plugin URI: 
+Description: 
+Version: 
+Author: Rodrigo
+Author URI: 
+License: 
+License URI: 
+*/
+
+
+
+
+function load_bootstrap(){
+    
+   //$path = plugin_dir_url( __FILE__ );
+
+
+    wp_enqueue_style('slider_css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css', date("h:i:s"));
+   
+
+
+    wp_enqueue_script('bootstrap_js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js', [], date("h:i:s"), true);
+
+   
+}
+
+
+add_action('wp_footer','load_bootstrap');
---
+++ script.js
@@ -0,0 +1,36 @@
+const el = document.querySelector(".carousel-inner");
+
+var wrapper = document.createElement("div");
+
+// insert wrapper before el in the DOM tree
+
+el.parentNode.insertBefore(wrapper, el);
+
+// move el into wrapper
+
+wrapper.appendChild(el);
+
+wrapper.id = "slider";
+
+var myCarousel = document.querySelector("#slider");
+
+var carousel = new bootstrap.Carousel(myCarousel);
+
+/*
+
+<div id="slider" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+  </div>
+</div>
+
+
+*/
